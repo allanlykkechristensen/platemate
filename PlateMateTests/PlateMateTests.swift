cc4d817a3f8bb68a9bf6bf0f3a047b192fdf34e8
Allan Lykke Christensen <allan.lykke.christensen@gmail.com>
2025-02-12 22:55:08 +0100

    Initial Commit

---
+++ PlateMateTests/PlateMateTests.swift
@@ -0,0 +1,17 @@
+//
+//  PlateMateTests.swift
+//  PlateMateTests
+//
+//  Created by Allan Lykke Christensen on 12/02/2025.
+//
+
+import Testing
+@testable import PlateMate
+
+struct PlateMateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
